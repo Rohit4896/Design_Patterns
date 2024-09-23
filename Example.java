0a45043dab455af45c016604ebbf3900b2a1b057
Rohit4896 <115925928+Rohit4896@users.noreply.github.com>
2024-09-23 11:34:12 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -10,52 +10,52 @@ public class Example {
 
         // Singleton Design Pattern
 
-        /*
-         * SingleTon single = SingleTon.getsingleTon();
-         * 
-         * SingleTon single2 = SingleTon.getsingleTon();
-         * 
-         * int firstHash = single.hashCode();
-         * 
-         * int secondHash = single2.hashCode();
-         * 
-         * if (firstHash == secondHash) {
-         * System.out.println("Both hashes and objects are equal");
-         * }
-         */
+        
+          SingleTon single = SingleTon.getsingleTon();
+          
+          SingleTon single2 = SingleTon.getsingleTon();
+          
+          int firstHash = single.hashCode();
+          
+          int secondHash = single2.hashCode();
+          
+          if (firstHash == secondHash) {
+         System.out.println("Both hashes and objects are equal");
+          }
+         
 
         // Breaking SingleTon Pattern Using Reflection API
 
-        /*
-         * SingleTon s1 = SingleTon.getsingleTon();
-         * System.out.println(s1.hashCode());
-         * 
-         * Constructor<SingleTon> con = SingleTon.class.getDeclaredConstructor();
-         * con.setAccessible(true);
-         * SingleTon s2 = con.newInstance();
-         * System.out.println(s2.hashCode());
-         * 
-         * 
-         */
+        
+          SingleTon s1 = SingleTon.getsingleTon();
+          System.out.println(s1.hashCode());
+          
+          Constructor<SingleTon> con = SingleTon.class.getDeclaredConstructor();
+          con.setAccessible(true);
+          SingleTon s2 = con.newInstance();
+          System.out.println(s2.hashCode());
+          
+          
+         
 
         // Breaking SingleTon Pattern Using Serialization/DeSerialization
 
-        /*
-         * SingleTon s3 = SingleTon.getsingleTon();
-         * ObjectOutputStream os = new ObjectOutputStream(new
-         * FileOutputStream("abc.ob"));
-         * os.writeObject(s3);
-         * 
-         * System.out.println("Serialization Done");
-         * 
-         * ObjectInputStream is = new ObjectInputStream(new FileInputStream("abc.ob"));
-         * SingleTon s4 = (SingleTon) is.readObject();
-         * 
-         * System.out.println(s3.hashCode());
-         * System.out.println(s4.hashCode());
-         * 
-         *
-         */
+        
+         SingleTon s3 = SingleTon.getsingleTon();
+          ObjectOutputStream os = new ObjectOutputStream(new
+          FileOutputStream("abc.ob"));
+          os.writeObject(s3);
+          
+          System.out.println("Serialization Done");
+          
+          ObjectInputStream is = new ObjectInputStream(new FileInputStream("abc.ob"));
+          SingleTon s4 = (SingleTon) is.readObject();
+          
+          System.out.println(s3.hashCode());
+          System.out.println(s4.hashCode());
+          
+         
+         
 
         // Breaking SingleTon Pattern Using cloning
 
